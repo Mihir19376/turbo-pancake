dc502e39ce923bc2e4587ed3635cdd4ec4a4c093
Mihir19376 <19376@wc.school.nz>
2022-04-18 09:33:45 +1200

    Create the start of my project (read description))
     - Create a player and player script, make it move and rotate
     - create a shooter and bullets that destroy themself on any impact
     - create an enemy thqt follow the player around and removes health from the player when they get hit by it. And the enemy also has health
     - implement a 3d tilemap system

---
+++ TopDownShooterGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float speed = 2.5f;
+    private float maxHealth = 10f;
+    public float currentHealth;
+
+    private Rigidbody enemyRb;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            Destroy(collision.gameObject);
+            currentHealth -= 1;
+        }
+
+    }
+}
---
+++ TopDownShooterGame/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float verticalInput;
+    public float speed = 10f;
+    public float rotateSpeed = 200f;
+    private float maxHealth = 10f;
+    public float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+            
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+
+        //if (currentHealth <= 0)
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            currentHealth -= 1;
+        }
+    }
+}
